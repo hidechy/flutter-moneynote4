93e66f735cf983b9321ea6b429d3cb9980763718
toyohide <hide.toyoda@gmail.com>
2022-12-28 09:00:56 +0900

    commit 20221228

--- lib/screens/_components/balance_sheet_alert.dart
+++ lib/screens/_components/balance_sheet_alert.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:moneynote4/models/balancesheet.dart';
 
 import '../../extensions/extensions.dart';
 import '../../state/app_param/app_param_notifier.dart';
@@ -106,9 +107,29 @@ class BalanceSheetAlert extends ConsumerWidget {
             .toDateTime()));
 
     for (var i = 0; i < balanceSheetState.length; i++) {
+      final data = balanceSheetState[i];
+
       list.add(
-        Row(
-          children: [Text(balanceSheetState[i].ym)],
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 10),
+              child: Text(data.ym),
+            ),
+            getAssetsWidget(data: data),
+            Container(
+              alignment: Alignment.topRight,
+              padding: const EdgeInsets.only(right: 10),
+              child: Text(data.assetsTotal.toString().toCurrency()),
+            ),
+            getCapitalWidget(data: data),
+            Container(
+              alignment: Alignment.topRight,
+              padding: const EdgeInsets.only(right: 10),
+              child: Text(data.capitalTotal.toString().toCurrency()),
+            ),
+          ],
         ),
       );
     }
@@ -116,6 +137,284 @@ class BalanceSheetAlert extends ConsumerWidget {
     return SingleChildScrollView(
       child: Column(
         children: list,
+      ),
+    );
+  }
+
+  //
+  Widget getAssetsWidget({required Balancesheet data}) {
+    return Container(
+      margin: const EdgeInsets.all(5),
+      padding: const EdgeInsets.all(5),
+      decoration: BoxDecoration(
+        color: Colors.blueAccent.withOpacity(0.3),
+        border: Border.all(
+          color: Colors.white.withOpacity(0.3),
+        ),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          const Text('【現金及び預金合計】'),
+          Table(
+            children: [
+              TableRow(
+                children: [
+                  Container(
+                    alignment: Alignment.topRight,
+                    child: Text(data.assets['assets_total_deposit_start']
+                        .toString()
+                        .toCurrency()),
+                  ),
+                  Container(
+                    alignment: Alignment.topRight,
+                    child: Text(
+                        '+ ${data.assets['assets_total_deposit_debit'].toString().toCurrency()}'),
+                  ),
+                  Container(
+                    alignment: Alignment.topRight,
+                    child: Text(
+                        '- ${data.assets['assets_total_deposit_credit'].toString().toCurrency()}'),
+                  ),
+                  Container(
+                    alignment: Alignment.topRight,
+                    child: Text(
+                        '= ${data.assets['assets_total_deposit_end'].toString().toCurrency()}'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          const Text('【売掛債権合計】'),
+          Table(
+            children: [
+              TableRow(
+                children: [
+                  Container(
+                    alignment: Alignment.topRight,
+                    child: Text(data.assets['assets_total_receivable_start']
+                        .toString()
+                        .toCurrency()),
+                  ),
+                  Container(
+                    alignment: Alignment.topRight,
+                    child: Text(
+                        '+ ${data.assets['assets_total_receivable_debit'].toString().toCurrency()}'),
+                  ),
+                  Container(
+                    alignment: Alignment.topRight,
+                    child: Text(
+                        '- ${data.assets['assets_total_receivable_credit'].toString().toCurrency()}'),
+                  ),
+                  Container(
+                    alignment: Alignment.topRight,
+                    child: Text(
+                        '= ${data.assets['assets_total_receivable_end'].toString().toCurrency()}'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          const Text('【有形固定資産合計】'),
+          Table(
+            children: [
+              TableRow(
+                children: [
+                  Container(
+                    alignment: Alignment.topRight,
+                    child: Text(data.assets['assets_total_fixed_start']
+                        .toString()
+                        .toCurrency()),
+                  ),
+                  Container(
+                    alignment: Alignment.topRight,
+                    child: Text(
+                        '+ ${data.assets['assets_total_fixed_debit'].toString().toCurrency()}'),
+                  ),
+                  Container(
+                    alignment: Alignment.topRight,
+                    child: Text(
+                        '- ${data.assets['assets_total_fixed_credit'].toString().toCurrency()}'),
+                  ),
+                  Container(
+                    alignment: Alignment.topRight,
+                    child: Text(
+                        '= ${data.assets['assets_total_fixed_end'].toString().toCurrency()}'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          const Text('【事業主貸合計】'),
+          Table(
+            children: [
+              TableRow(
+                children: [
+                  Container(
+                    alignment: Alignment.topRight,
+                    child: Text(data.assets['assets_total_lending_start']
+                        .toString()
+                        .toCurrency()),
+                  ),
+                  Container(
+                    alignment: Alignment.topRight,
+                    child: Text(
+                        '+ ${data.assets['assets_total_lending_debit'].toString().toCurrency()}'),
+                  ),
+                  Container(
+                    alignment: Alignment.topRight,
+                    child: Text(
+                        '- ${data.assets['assets_total_lending_credit'].toString().toCurrency()}'),
+                  ),
+                  Container(
+                    alignment: Alignment.topRight,
+                    child: Text(
+                        '= ${data.assets['assets_total_lending_end'].toString().toCurrency()}'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  ///
+  Widget getCapitalWidget({required Balancesheet data}) {
+    return Container(
+      margin: const EdgeInsets.all(5),
+      padding: const EdgeInsets.all(5),
+      decoration: BoxDecoration(
+        color: Colors.greenAccent.withOpacity(0.3),
+        border: Border.all(
+          color: Colors.white.withOpacity(0.3),
+        ),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          const Text('【流動負債合計】'),
+          Table(
+            children: [
+              TableRow(
+                children: [
+                  Container(
+                    alignment: Alignment.topRight,
+                    child: Text(data.capital['capital_total_liabilities_start']
+                        .toString()
+                        .toCurrency()),
+                  ),
+                  Container(
+                    alignment: Alignment.topRight,
+                    child: Text(
+                        '- ${data.capital['capital_total_liabilities_debit'].toString().toCurrency()}'),
+                  ),
+                  Container(
+                    alignment: Alignment.topRight,
+                    child: Text(
+                        '+ ${data.capital['capital_total_liabilities_credit'].toString().toCurrency()}'),
+                  ),
+                  Container(
+                    alignment: Alignment.topRight,
+                    child: Text(
+                        '= ${data.capital['capital_total_liabilities_end'].toString().toCurrency()}'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          const Text('【事業主借合計】'),
+          Table(
+            children: [
+              TableRow(
+                children: [
+                  Container(
+                    alignment: Alignment.topRight,
+                    child: Text(data.capital['capital_total_borrow_start']
+                        .toString()
+                        .toCurrency()),
+                  ),
+                  Container(
+                    alignment: Alignment.topRight,
+                    child: Text(
+                        '- ${data.capital['capital_total_borrow_debit'].toString().toCurrency()}'),
+                  ),
+                  Container(
+                    alignment: Alignment.topRight,
+                    child: Text(
+                        '+ ${data.capital['capital_total_borrow_credit'].toString().toCurrency()}'),
+                  ),
+                  Container(
+                    alignment: Alignment.topRight,
+                    child: Text(
+                        '= ${data.capital['capital_total_borrow_end'].toString().toCurrency()}'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          const Text('【元入金】'),
+          Table(
+            children: [
+              TableRow(
+                children: [
+                  Container(
+                    alignment: Alignment.topRight,
+                    child: Text(data.capital['capital_total_principal_start']
+                        .toString()
+                        .toCurrency()),
+                  ),
+                  Container(
+                    alignment: Alignment.topRight,
+                    child: Text(
+                        '- ${data.capital['capital_total_principal_debit'].toString().toCurrency()}'),
+                  ),
+                  Container(
+                    alignment: Alignment.topRight,
+                    child: Text(
+                        '+ ${data.capital['capital_total_principal_credit'].toString().toCurrency()}'),
+                  ),
+                  Container(
+                    alignment: Alignment.topRight,
+                    child: Text(
+                        '= ${data.capital['capital_total_principal_end'].toString().toCurrency()}'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          const Text('【控除前所得合計】'),
+          Table(
+            children: [
+              TableRow(
+                children: [
+                  Container(
+                    alignment: Alignment.topRight,
+                    child: Text(data.capital['capital_total_income_start']
+                        .toString()
+                        .toCurrency()),
+                  ),
+                  Container(
+                    alignment: Alignment.topRight,
+                    child: Text(
+                        '- ${data.capital['capital_total_income_debit'].toString().toCurrency()}'),
+                  ),
+                  Container(
+                    alignment: Alignment.topRight,
+                    child: Text(
+                        '+ ${data.capital['capital_total_income_credit'].toString().toCurrency()}'),
+                  ),
+                  Container(
+                    alignment: Alignment.topRight,
+                    child: Text(
+                        '= ${data.capital['capital_total_income_end'].toString().toCurrency()}'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
