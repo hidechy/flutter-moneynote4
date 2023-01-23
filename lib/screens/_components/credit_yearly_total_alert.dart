2e9bbec7392bba19928bdf802042798d3651def4
toyoda-hideyuki <toyoda-hideyuki@sunited.jp>
2023-01-23 16:18:16 +0900

    commit 20220123

--- lib/screens/_components/credit_yearly_total_alert.dart
+++ lib/screens/_components/credit_yearly_total_alert.dart
@@ -75,7 +75,7 @@ class CreditYearlyTotalAlert extends ConsumerWidget {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             SizedBox(
-              width: _context.screenSize.width * 0.5,
+              width: _context.screenSize.width * 0.55,
               child: Text(
                 creditYearlyTotalState[i].item,
                 maxLines: 1,
