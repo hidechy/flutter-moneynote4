ea1c804f51b215e97a6f0f88d8e63c289e4b01a1
toyohide <hide.toyoda@gmail.com>
2023-06-24 09:17:08 +0900

    commit 20230624 credit_company_alert

--- lib/screens/_components/credit_company_alert.dart
+++ lib/screens/_components/credit_company_alert.dart
@@ -1,212 +1,86 @@
-// ignore_for_file: must_be_immutable, non_constant_identifier_names
+// ignore_for_file: must_be_immutable
 
 import 'package:flutter/material.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-import '../../extensions/extensions.dart';
-import '../../state/app_param/app_param_notifier.dart';
-import '../../state/device_info/device_info_notifier.dart';
-import '../../utility/utility.dart';
-import '../../viewmodel/credit_notifier.dart';
+import 'pages/credit_company_page.dart';
 
-class CreditCompanyAlert extends ConsumerWidget {
+class TabInfo {
+  TabInfo(this.label, this.widget);
+
+  String label;
+  Widget widget;
+}
+
+class CreditCompanyAlert extends StatelessWidget {
   CreditCompanyAlert({super.key, required this.date});
 
   final DateTime date;
 
-  List<String> creditCompany = ['uc', 'rakuten', 'sumitomo', 'amex'];
-  Map<String, List<Map<String, dynamic>>> creditCompanyMap = {};
-
-  final Utility _utility = Utility();
-
-  late WidgetRef _ref;
+  List<TabInfo> tabs = [];
 
   ///
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    _ref = ref;
+  Widget build(BuildContext context) {
+    makeTab();
 
-    final yearWidgetList = makeYearWidgetList();
+    return DefaultTabController(
+      length: tabs.length,
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(50),
+          child: AppBar(
+            backgroundColor: Colors.transparent,
+            //-------------------------//これを消すと「←」が出てくる（消さない）
+            leading: const Icon(
+              Icons.check_box_outline_blank,
+              color: Colors.transparent,
+            ),
+            //-------------------------//これを消すと「←」が出てくる（消さない）
 
-    final deviceInfoState = ref.read(deviceInfoProvider);
+            bottom: TabBar(
+              isScrollable: true,
+              indicatorColor: Colors.blueAccent,
+              tabs: tabs.map((TabInfo tab) {
+                return Tab(text: tab.label);
+              }).toList(),
+            ),
 
-    return AlertDialog(
-      titlePadding: EdgeInsets.zero,
-      contentPadding: EdgeInsets.zero,
-      backgroundColor: Colors.transparent,
-      insetPadding: EdgeInsets.zero,
-      content: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        width: double.infinity,
-        height: double.infinity,
-        child: SingleChildScrollView(
-          child: DefaultTextStyle(
-            style: const TextStyle(fontSize: 12),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(height: 20),
-                Container(width: context.screenSize.width),
-
-                //----------//
-                if (deviceInfoState.model == 'iPhone')
-                  _utility.getFileNameDebug(name: runtimeType.toString()),
-                //----------//
-
-                Row(children: yearWidgetList),
-                const SizedBox(height: 20),
-                displayCreditCompany(),
-              ],
+            flexibleSpace: const DecoratedBox(
+              decoration: BoxDecoration(
+                color: Colors.transparent,
+              ),
             ),
           ),
+        ),
+        body: TabBarView(
+          children: tabs.map((tab) => tab.widget).toList(),
         ),
       ),
     );
   }
 
   ///
-  List<Widget> makeYearWidgetList() {
-    final CreditCompanyAlertSelectYear = _ref.watch(
-      appParamProvider.select((value) => value.CreditCompanyAlertSelectYear),
-    );
+  void makeTab() {
+    tabs = [];
 
-    final yearList = <Widget>[];
-    for (var i = date.yyyy.toInt(); i >= 2020; i--) {
-      yearList.add(
-        GestureDetector(
-          onTap: () {
-            _ref
-                .watch(appParamProvider.notifier)
-                .setCreditCompanyAlertSelectYear(year: i);
-          },
-          child: Container(
-            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 5),
-            margin: const EdgeInsets.only(right: 10),
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.white.withOpacity(0.5)),
-              color: (i == CreditCompanyAlertSelectYear)
-                  ? Colors.yellowAccent.withOpacity(0.2)
-                  : null,
-            ),
-            child: Text(i.toString()),
-          ),
-        ),
-      );
+    final list = <int>[];
+
+    for (var i = 2020; i <= DateTime.now().year; i++) {
+      list.add(i);
     }
 
-    return yearList;
-  }
-
-  ///
-  Widget displayCreditCompany() {
-    final CreditCompanyAlertSelectYear = _ref.watch(
-      appParamProvider.select((value) => value.CreditCompanyAlertSelectYear),
-    );
-
-    final creditCompanyState = _ref.watch(
-      creditCompanyProvider(DateTime(CreditCompanyAlertSelectYear)),
-    );
-
-    final list = <Widget>[];
-
-    creditCompanyState.forEach((element) {
-      final list2 = <Widget>[];
-
-      var total = 0;
-      element.list.forEach((element2) {
-        list2.add(
-          Container(
-            padding: const EdgeInsets.symmetric(vertical: 5),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(element2.company),
-                Row(
-                  children: [
-                    Text(element2.sum.toString().toCurrency()),
-                    const SizedBox(width: 20),
-                    getCreditMark(kind: element2.company),
-                  ],
-                ),
-              ],
+    list
+      ..sort((a, b) => -1 * a.compareTo(b))
+      ..forEach((element) {
+        tabs.add(
+          TabInfo(
+            element.toString(),
+            CreditCompanyPage(
+              date: DateTime(element),
             ),
           ),
         );
-
-        total += element2.sum;
       });
-
-      list2
-        ..add(
-          Divider(
-            color: Colors.white.withOpacity(0.3),
-            thickness: 1,
-          ),
-        )
-        ..add(
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(),
-              Text(
-                total.toString().toCurrency(),
-                style: const TextStyle(color: Colors.yellowAccent),
-              ),
-            ],
-          ),
-        )
-        ..add(const SizedBox(height: 30));
-
-      list.add(
-        Container(
-          padding: const EdgeInsets.symmetric(vertical: 3),
-          decoration: BoxDecoration(
-            border: Border(
-              bottom: BorderSide(
-                color: Colors.white.withOpacity(0.3),
-              ),
-            ),
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(element.ym),
-              Row(
-                children: [
-                  Container(width: 20),
-                  Expanded(
-                    child: Column(
-                      children: list2,
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-      );
-    });
-
-    return SingleChildScrollView(
-      child: Column(
-        children: list,
-      ),
-    );
-  }
-
-  ///
-  Widget getCreditMark({required String kind}) {
-    switch (kind) {
-      case 'uc':
-        return const Icon(Icons.credit_card, color: Colors.redAccent);
-      case 'rakuten':
-        return const Icon(Icons.credit_card, color: Colors.orangeAccent);
-      case 'amex':
-        return const Icon(Icons.credit_card, color: Colors.purpleAccent);
-      case 'sumitomo':
-        return const Icon(Icons.credit_card, color: Colors.greenAccent);
-    }
-
-    return const Icon(Icons.credit_card, color: Colors.white);
   }
 }
---
+++ lib/screens/_components/pages/credit_company_page.dart
@@ -0,0 +1,173 @@
+// ignore_for_file: must_be_immutable
+
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import '../../../extensions/extensions.dart';
+import '../../../state/device_info/device_info_notifier.dart';
+import '../../../utility/utility.dart';
+import '../../../viewmodel/credit_notifier.dart';
+
+class CreditCompanyPage extends ConsumerWidget {
+  CreditCompanyPage({super.key, required this.date});
+
+  final DateTime date;
+
+  final Utility _utility = Utility();
+
+  late WidgetRef _ref;
+
+  ///
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    _ref = ref;
+
+    final deviceInfoState = ref.read(deviceInfoProvider);
+
+    return AlertDialog(
+      titlePadding: EdgeInsets.zero,
+      contentPadding: EdgeInsets.zero,
+      backgroundColor: Colors.transparent,
+      insetPadding: EdgeInsets.zero,
+      content: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        width: double.infinity,
+        height: double.infinity,
+        child: DefaultTextStyle(
+          style: const TextStyle(fontSize: 12),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 20),
+              Container(width: context.screenSize.width),
+
+              //----------//
+              if (deviceInfoState.model == 'iPhone') _utility.getFileNameDebug(name: runtimeType.toString()),
+              //----------//
+
+              Expanded(child: displayCreditCompany()),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  ///
+  Widget displayCreditCompany() {
+    final creditCompanyState = _ref.watch(creditCompanyProvider(date));
+
+    final list = <Widget>[];
+
+    creditCompanyState.forEach((element) {
+      final list2 = <Widget>[];
+
+      var addList = false;
+
+      element.list.forEach((element2) {
+        if (element2.sum.toString().toInt() > 0) {
+          addList = true;
+        }
+      });
+
+      var total = 0;
+
+      if (addList) {
+        element.list.forEach((element2) {
+          list2.add(
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 5),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(element2.company),
+                  Row(
+                    children: [
+                      Text(element2.sum.toString().toCurrency()),
+                      const SizedBox(width: 20),
+                      getCreditMark(kind: element2.company),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          );
+
+          total += element2.sum;
+        });
+
+        list2
+          ..add(
+            Divider(
+              color: Colors.white.withOpacity(0.3),
+              thickness: 1,
+            ),
+          )
+          ..add(
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(),
+                Text(
+                  total.toString().toCurrency(),
+                  style: const TextStyle(color: Colors.yellowAccent),
+                ),
+              ],
+            ),
+          )
+          ..add(const SizedBox(height: 30));
+
+        list.add(
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 3),
+            decoration: BoxDecoration(
+              border: Border(
+                bottom: BorderSide(
+                  color: Colors.white.withOpacity(0.3),
+                ),
+              ),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(element.ym),
+                Row(
+                  children: [
+                    Container(width: 20),
+                    Expanded(
+                      child: Column(
+                        children: list2,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        );
+      }
+    });
+
+    return SingleChildScrollView(
+      child: Column(
+        children: list,
+      ),
+    );
+  }
+
+  ///
+  Widget getCreditMark({required String kind}) {
+    switch (kind) {
+      case 'uc':
+        return const Icon(Icons.credit_card, color: Colors.redAccent);
+      case 'rakuten':
+        return const Icon(Icons.credit_card, color: Colors.orangeAccent);
+      case 'amex':
+        return const Icon(Icons.credit_card, color: Colors.purpleAccent);
+      case 'sumitomo':
+        return const Icon(Icons.credit_card, color: Colors.greenAccent);
+    }
+
+    return const Icon(Icons.credit_card, color: Colors.white);
+  }
+}
