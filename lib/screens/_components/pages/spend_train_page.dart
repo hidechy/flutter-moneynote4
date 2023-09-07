83cbf1360e6d38d6455d83c61bd587ee34f44eb6
toyohide <hide.toyoda@gmail.com>
2023-09-07 21:24:23 +0900

    20230907 spend page tab container

--- lib/screens/_components/pages/spend_train_page.dart
+++ lib/screens/_components/pages/spend_train_page.dart
@@ -1,13 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class SpendTrainPage extends StatelessWidget {
+import '../../../extensions/extensions.dart';
+import '../../../state/train/train_notifier.dart';
+
+class SpendTrainPage extends ConsumerWidget {
   const SpendTrainPage({super.key, required this.date});
 
   final DateTime date;
 
   ///
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+  Widget build(BuildContext context, WidgetRef ref) {
+    final trainMap = ref.watch(trainProvider.select((value) => value.trainMap));
+
+    return SizedBox(
+      width: context.screenSize.width,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          (trainMap[date.yyyymmdd] != null)
+              ? Text(trainMap[date.yyyymmdd]!.station, style: const TextStyle(fontSize: 10))
+              : Container(),
+        ],
+      ),
+    );
   }
 }
